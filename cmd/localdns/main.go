ad510c7ce5d2c26f209754586249ee9b00ac21f6
Nanaka <luminovoez@gmail.com>
2026-06-24 18:18:39 +0800

    localdns: refactor to pkg/localdns, add flag-based config

--- cmd/localdns/main.go
+++ cmd/localdns/main.go
@@ -1,26 +1,18 @@
 package main
 
 import (
+	"flag"
 	"log"
-	"os"
-	"strconv"
 
-	"github.com/Hana-ame/wintools/test/localdns"
+	"github.com/Hana-ame/wintools/pkg/localdns"
 )
 
 func main() {
-	dohEndpoint := os.Getenv("DOH_ENDPOINT")
-	if dohEndpoint == "" {
-		
-	}
-	port := 5353
-	if p := os.Getenv("DNS_PORT"); p != "" {
-		if n, err := strconv.Atoi(p); err == nil {
-			port = n
-		}
-	}
+	dohEndpoint := flag.String("doh", "https://moonchan.xyz/doh", "DoH endpoint URL")
+	port := flag.Int("port", 5353, "listen UDP port")
+	flag.Parse()
 
-	if err := localdns.Run(dohEndpoint, port); err != nil {
+	if err := localdns.Run(*dohEndpoint, *port); err != nil {
 		log.Fatalf("localdns: %v", err)
 	}
 }
